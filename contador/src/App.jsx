9725e50851454b8786f0dee85ef99690e978d1ea
mateo19 <mateojlopez@gmail.com>
2023-04-06 17:50:45 -0500

    update background-color and button color

--- contador/src/App.jsx
+++ contador/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import './App.css';
-import imagen from './img/images.png';
+import imagen from './img/contador.png';
 import { Boton } from './components/Boton/Boton';
 import { Contador } from './components/contador/Contador';
 
 function App() {
   const [numClicks, setNumClicks] = useState(0)
 
-  const manejarClick = () => {
+  const aumentarContador = () => {
     setNumClicks(numClicks + 1);
+  };
+
+  const disminuirContador = () => {
+    if(numClicks == 0){
+      setNumClicks(0);
+    }else{
+      setNumClicks(numClicks - 1);
+    }
   };
 
   const reiniciarContador = () => {
@@ -28,12 +36,17 @@ function App() {
             numClicks={numClicks}
           />
           <Boton 
-            texto='click'
+            texto='⇧'
             esBotonDeClick={true}
-            manejarClick={manejarClick}
+            manejarClick={aumentarContador}
           />
           <Boton 
-            texto='reiniciar'
+            texto='⇩'
+            esBotonDeClick={true}
+            manejarClick={disminuirContador}
+          />
+          <Boton 
+            texto='Reiniciar'
             esBotonDeClick={false}
             manejarClick={reiniciarContador}
           />
